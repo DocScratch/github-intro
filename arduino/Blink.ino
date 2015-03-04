7e9bcd7728016d3a8d2a0c210583af84559e6cdb
Andrew Johnston <thewhitetextguy@gmail.com>
2015-03-04 10:04:59 -0800

    Update Blink.ino

--- arduino/Blink.ino
+++ arduino/Blink.ino
@@ -1,15 +1,15 @@
 //Did it work?
-int led = 13;
+int led = 13; // Chooses the LED pin
 
 void setup() {                
 
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);   // sets LED as output  
 }
 
 
-void loop() {
-  digitalWrite(led, HIGH);   
-  delay(1000);              
-  digitalWrite(led, LOW);    
-  delay(1000);              
+void loop() { // starts the loop
+  digitalWrite(led, HIGH); // LED 'on'   
+  delay(1000);   // sets delay           
+  digitalWrite(led, LOW);    // LED 'off'
+  delay(1000); // sets delay             
 }
